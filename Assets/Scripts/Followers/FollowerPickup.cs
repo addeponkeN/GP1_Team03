83468755fbeb51de1a140dafec485e062fe9e500
MagnuthFG <magnus.andersson@edu.futuregames.nu>
2022-10-11 16:18:25 +0200

    Worked on follower logic
    Added follower pickup script and started on player followers script

--- Assets/Scripts/Followers/FollowerPickup.cs
+++ Assets/Scripts/Followers/FollowerPickup.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(SphereCollider))]
-public class PeoplePickup : MonoBehaviour
+public class FollowerPickup : MonoBehaviour
 {
     [Header("Circle Settings")]
     [SF] private int _zones = 3;
     [SF] private int _minPickupCount = 1;
     [SF] private LayerMask _playerLayer = 1 << 0;
-    [SF] private List<GameObject> _people = null;
+    [SF] private List<Transform> _people = null;
 
     [Header("References")]
     [SF] private InputActionReference _pickupInput = null;
@@ -19,66 +19,79 @@ public class PeoplePickup : MonoBehaviour
 
     private Transform _player = null;
 
-    // INITIALISATION
+// INITIALISATION
 
     /// <summary>
-    /// 
+    /// Initialises the collider
     /// </summary>
-    private void Awake()
-    {
+    private void Awake(){
         _collider.isTrigger = true;
     }
 
     /// <summary>
-    /// 
+    /// Subscribes to interaction input
     /// </summary>
-    private void OnEnable()
-    {
+    private void OnEnable(){
         //_pickupInput.action.canceled += ctx => OnInteractInput(ctx);
     }
 
     /// <summary>
-    /// 
+    /// Unsubscribes from interaction input
     /// </summary>
-    private void OnDisable()
-    {
+    private void OnDisable(){
         //_pickupInput.action.canceled -= OnInteractInput;
     }
 
     // TEMP
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
+    private void Update(){
+        if (Input.GetKeyUp(KeyCode.Space)){
             OnInteractInput(default(CallbackContext));
+        }
     }
 
-    // INPUT EVENTS
+// INPUT EVENTS
 
     /// <summary>
     /// On interact button released
     /// </summary>
-    private void OnInteractInput(CallbackContext context)
-    {
+    private void OnInteractInput(CallbackContext context){
         if (_player == null) return;
 
         var distance = Vector3.Distance(
             _player.position, transform.position
         );
+        
+        var radius = _collider.radius;
+        var difference = radius / _zones;
 
-        var difference = _collider.radius / _zones;
-        var percent = Mathf.InverseLerp(0, _collider.radius, distance);
-        var count = Mathf.Lerp(_minPickupCount, _people.Count, percent);
+        for (int i = 0; i <= _zones; i++){
+            bool less = distance < radius - (difference * i);
+            bool greater = distance > radius - (difference * (i + 1));
+            if (!less || !greater) continue;
 
+            var percent = Mathf.InverseLerp(0, _zones, i + 1);
+            var count =  (int)Mathf.Lerp(_minPickupCount, _people.Count, percent);
+            var recruited = new Transform[count];
 
+            for (int j = count - 1; j >= 0; j--){
+                var person = _people[j];
+                if (person == null) continue;
+                
+                recruited[j] = person;
+                _people.RemoveAt(j);
+            }
+
+            var followers = _player.GetComponent<PlayerFollowers>();
+            followers.AddFollowers(recruited);
+        }
     }
 
-    // TRIGGERING
+// TRIGGERING
 
     /// <summary>
     /// If the player enters the pickup circle
     /// </summary>
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other){
         var layer = other.gameObject.layer;
         if (((1 << layer) & _playerLayer) == 0) return;
 
@@ -88,20 +101,18 @@ public class PeoplePickup : MonoBehaviour
     /// <summary>
     /// If the player exits the pickup circle
     /// </summary>
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit(Collider other){
         if (other.transform != _player) return;
         _player = null;
     }
 
-    // DEBUGGING
+// DEBUGGING
 #if UNITY_EDITOR
 
     /// <summary>
     /// Draws a percent based circle around the pickup
     /// </summary>
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos(){
         if (_collider == null) return;
         var radius = _collider.radius;
         var difference = 1f / _zones;
@@ -118,8 +129,7 @@ public class PeoplePickup : MonoBehaviour
     /// <summary>
     /// Draws a circle around the pickup position
     /// </summary>
-    private void DrawCircle(Color colour, float radius)
-    {
+    private void DrawCircle(Color colour, float radius){
         Gizmos.color = colour;
 
         var centre = transform.position;
