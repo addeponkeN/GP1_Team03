147562ed60a5a0f932f2146f6851602c2b4d4bfe
addeponkeN <addeponken@hotmail.com>
2022-10-12 13:39:30 +0200

    updated playerstats range values

--- Assets/Scripts/PlayerStatContainer.cs
+++ Assets/Scripts/PlayerStatContainer.cs
@@ -10,19 +10,26 @@ public class PlayerStatContainer : ScriptableObject
 //  PLAYER STAT FIELDS
 
 
-    [TooltipAttribute("The movement acceleration of the bike")] [Range(1f, 500f)]
+    [Header("Movement")] 
+    [TooltipAttribute("The movement acceleration of the bike")] 
+    [Range(1f, 30f)]
     public float MovementAcceleration = 5f;
 
 
-    [TooltipAttribute("The maximum speed of the bike")] [Range(2f, 500f)]
+    [TooltipAttribute("The maximum speed of the bike")] 
+    [Range(2f, 50f)]
     public float MaxMoveSpeed = 50f;
 
 
-    [TooltipAttribute("The rotation speed of the bike")] [Range(1f, 50f)]
+    [Space]
+    [Header("Rotation")] 
+    [TooltipAttribute("The rotation speed of the bike")] 
+    [Range(1f, 50f)]
     public float RotationSpeed = 8f;
 
 
-    [TooltipAttribute("The maximum rotation speed of the bike")] [Range(1f, 50f)]
+    [TooltipAttribute("The maximum rotation speed of the bike")] 
+    [Range(1f, 50f)]
     public float MaxRotationSpeed = 8f;
 
 
@@ -32,8 +39,18 @@ public class PlayerStatContainer : ScriptableObject
     [Range(0f, 1f)]
     public float RotationSpeedModifier = .25f;
 
+
+    [Space] 
+    [Header("Boosting")]
+    [TooltipAttribute("Amount of time the boost should last")] 
+    [Range(0f, 5f)]
+    public float BoostTime = 1.5f;
     
-    
+    [TooltipAttribute("Amount multiplied by speed")] 
+    [Range(1f, 10f)]
+    public float BoostAmount = 3f;
+
+
     /// <summary>
     /// Setup the stat container
     /// </summary>
@@ -47,21 +64,21 @@ public class PlayerStatContainer : ScriptableObject
 [Serializable]
 public class Stat<T>
 {
-     public static implicit operator T(Stat<T> stat) => stat.Value; 
-     
-     public event Action<Stat<T>> ValueChangedEvent;
-     
-     [SerializeField] public T Value;
+    public static implicit operator T(Stat<T> stat) => stat.Value;
 
-     public Stat(T value)
-     {
-         Value = value;
-         ValueChangedEvent = null;
-     }
+    public event Action<Stat<T>> ValueChangedEvent;
 
-     public void SetValue(T value)
-     {
-         Value = value;
-         ValueChangedEvent?.Invoke(this);
-     }
+    [SerializeField] public T Value;
+
+    public Stat(T value)
+    {
+        Value = value;
+        ValueChangedEvent = null;
+    }
+
+    public void SetValue(T value)
+    {
+        Value = value;
+        ValueChangedEvent?.Invoke(this);
+    }
 }
