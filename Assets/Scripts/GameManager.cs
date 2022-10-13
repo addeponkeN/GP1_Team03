cec73fbc8b5f68fd7b78a7f05257958d71e60a38
addeponkeN <addeponken@hotmail.com>
2022-10-13 13:15:46 +0200

    effect meta. GameManager win/lost condition(beta).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Start()
+    private static GameRules _defaultRules;
+
+    public GameRules Rules;
+
+    private Player _player;
+    private PlayerTimer _playerTimer;
+
+    private void Awake()
     {
+        _defaultRules = ScriptableObject.CreateInstance<GameRules>();
+        if(Rules == null)
+            Rules = _defaultRules;
     }
 
-    private void Update()
+    private void Start()
     {
+        _player = GameCore.Get.Player;
+        if(_player == null) return;
+    }
+
+    public void OnFollowerAdded()
+    {
+        if(_player == null) return;
+        CheckWinCondition();
+    }
+
+    public void OnFollowerLost()
+    {
+        if(_player == null) return;
+        var followers = _player.GetComponent<PlayerFollowers>();
+    }
+
+    private bool CheckLoseCondition()
+    {
+        return false;
+    }
+    
+    private bool CheckWinCondition()
+    {
+        var followers = _player.GetComponent<PlayerFollowers>();
+        return followers.Count >= Rules.FollowerWinCount;
     }
 }
---
+++ Assets/Scripts/PlayerTimer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerTimer : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
