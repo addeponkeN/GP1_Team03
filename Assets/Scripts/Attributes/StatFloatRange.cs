21fcb4fefc5a8f2430b5b4124cdf52b00f6d16fd
Chugoolsson <chugoolsson@gmail.com>
2022-10-13 15:31:44 +0200

    lvl allmost finished

--- Assets/Scripts/Attributes/StatFloatRange.cs
+++ Assets/Scripts/Attributes/StatFloatRange.cs
@@ -1,48 +1,48 @@
-using System;
-using UnityEditor;
-using UnityEngine;
+//using System;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace Attributes
-{
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-    public class StatFloatRange : PropertyAttribute
-    {
-        public readonly float Min;
-        public readonly float Max;
+//namespace Attributes
+//{
+//    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+//    public class StatFloatRange : PropertyAttribute
+//    {
+//        public readonly float Min;
+//        public readonly float Max;
 
-        public StatFloatRange(float min, float max)
-        {
-            Min = min;
-            Max = max;
-        }
-    }
+//        public StatFloatRange(float min, float max)
+//        {
+//            Min = min;
+//            Max = max;
+//        }
+//    }
 
-    [CustomPropertyDrawer(typeof(StatFloatRange))]
-    public class StatFloatDrawer : PropertyDrawer
-    {
-        private float _value;
+//    [CustomPropertyDrawer(typeof(StatFloatRange))]
+//    public class StatFloatDrawer : PropertyDrawer
+//    {
+//        private float _value;
 
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            var range = (StatFloatRange)attribute;
+//        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+//        {
+//            var range = (StatFloatRange)attribute;
 
-            // var asset = new SerializedObject(property.objectReferenceValue);
-            // var prop = asset.FindProperty("Value");
+//            // var asset = new SerializedObject(property.objectReferenceValue);
+//            // var prop = asset.FindProperty("Value");
             
-            var prop = property.FindPropertyRelative("Value");
+//            var prop = property.FindPropertyRelative("Value");
             
-            _value = EditorGUI.Slider(position, label, _value, range.Min, range.Max);
+//            _value = EditorGUI.Slider(position, label, _value, range.Min, range.Max);
 
-            prop.floatValue = _value;
+//            prop.floatValue = _value;
             
-            property.serializedObject.Update();
-            property.serializedObject.ApplyModifiedProperties();
+//            property.serializedObject.Update();
+//            property.serializedObject.ApplyModifiedProperties();
                 
-            // prop.floatValue = _value;
-            // property.floatValue = _value;
-        }
-    }
-}
+//            // prop.floatValue = _value;
+//            // property.floatValue = _value;
+//        }
+//    }
+//}
 
 
 
