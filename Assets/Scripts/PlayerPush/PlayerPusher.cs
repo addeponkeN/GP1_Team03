c3f8f006993db589b49267bdf921b5cde584aed0
addeponkeN <addeponken@hotmail.com>
2022-10-17 11:32:01 +0200

    added stop function to accelerator. added TagHelper. added LayerHelper. added "jump pad". fixed movementcontroller.

---
+++ Assets/Scripts/PlayerPush/PlayerPusher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PlayerControllers.Controllers;
+using UnityEngine;
+
+namespace PlayerPush
+{
+    public class PlayerPusher : MonoBehaviour
+    {
+        /// <summary>
+        /// Direction of the pusher
+        /// </summary>
+        public Vector3 Direction = Vector3.up;
+
+        /// <summary>
+        /// The push force
+        /// </summary>
+        public float Power = 30f;
+
+        /// <summary>
+        /// The distance the player be pushed 
+        /// </summary>
+        public float Distance = 5f;
+
+        private LayerMask _playerMask = 1 << 6;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(((1 << other.gameObject.layer) & _playerMask) == 0)
+                return;
+
+            var player = other.gameObject.GetComponent<Player>();
+            // StopPlayer(player);
+
+            var body = player.GetComponent<Rigidbody>();
+            body.MovePosition(body.position + Direction * 0.1f);
+            body.AddForce(Direction * Power);
+        }
+
+        private void StopPlayer(Player player)
+        {
+            var move = player.ControllerManager.GetController<MovementController>();
+            move.Stop();
+            // move.Enabled = false;
+        }
+    }
+}
