b3096e7a6ebc34670d66392fb22cbfe5164ebc29
MagnuthFG <magnus.andersson@edu.futuregames.nu>
2022-10-18 16:18:17 +0200

    Added pickup poffs

--- Assets/Scripts/Followers/PlayerFollowerEvents.cs
+++ Assets/Scripts/Followers/PlayerFollowerEvents.cs
@@ -1,21 +1,44 @@
 using SF = UnityEngine.SerializeField;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerFollowerEvents : MonoBehaviour
 {
     [SF] private GameObject _pickupFXPrefab = null;
 
-    // Create pool for pickup fx and then invoke(fx play time) to disable it when done
+    private Queue<ParticleSystem> _pickupFXPool = null;
 
+// INITIALISATION
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake(){
+        InitFxPool(_pickupFXPrefab, ref _pickupFXPool);
+    }
+
+    /// <summary>
+    /// Initialises a new particle pool
+    /// </summary>
+    private void InitFxPool(GameObject prefab, ref Queue<ParticleSystem> pool) {
+        pool = new Queue<ParticleSystem>();
+
+        for (int i = 0; i < 6; i++){
+            var fxObj = Instantiate(_pickupFXPrefab, Vector3.zero, Quaternion.identity);
+            var fxSys = fxObj.GetComponent<ParticleSystem>();
+            pool.Enqueue(fxSys);
+        }
+    }
+
+// FOLLOWER EVENTS
 
     /// <summary>
     /// On follower picked up event
     /// </summary>
     public void OnPickedUp(Follower follower){
         if (follower == null) return;
-
-        //var tfm = follower.Transform;
-        //Instantiate(_pickupFXPrefab, tfm.position, tfm.rotation);
+        var tfm = follower.Transform;
+        PlayFX(tfm, ref _pickupFXPool);
     }
 
     /// <summary>
@@ -32,5 +55,20 @@ public class PlayerFollowerEvents : MonoBehaviour
     public void OnRemoved(Follower follower){
         if (follower == null) return;
         Destroy(follower.GameObject);
+    }
+
+// EFFECTS
+
+    /// <summary>
+    /// Plays the first available particle
+    /// </summary>
+    private void PlayFX(Transform follower, ref Queue<ParticleSystem> pool){
+        if (pool.Count == 0) return;
+        var fx = pool.Dequeue();
+
+        fx.transform.position = follower.position;
+        fx.Play();
+
+        pool.Enqueue(fx);
     }
 }
