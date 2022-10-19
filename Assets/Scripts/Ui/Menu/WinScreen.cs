129470b799fe4188ee246ad14467dbb653d433a6
Kezze16 <kez.fds@gmail.com>
2022-10-19 17:06:39 +0200

    WiningText Scene manger

--- Assets/Scripts/Ui/Menu/WinScreen.cs
+++ Assets/Scripts/Ui/Menu/WinScreen.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RestartGame()
     {
-        
+        SceneManager.LoadScene("MainLevel");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReturnToMainMenu()
     {
-        
+        SceneManager.LoadScene("START");
     }
 }
