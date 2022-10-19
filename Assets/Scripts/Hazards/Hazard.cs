f1a8ea5f538d8ec8424257dae97d72666b452929
MagnuthFG <magnus.andersson@edu.futuregames.nu>
2022-10-19 11:20:16 +0200

    Improved hazards and followers

--- Assets/Scripts/Hazards/Hazard.cs
+++ Assets/Scripts/Hazards/Hazard.cs
@@ -1,19 +1,23 @@
 using SF = UnityEngine.SerializeField;
-using UnityEngine;
 using PlayerControllers.Controllers;
-using Jellybeans.Updates;
+using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
     [SF] private bool _canBeDestroyed = true;
-    [SF] private float _collisionTimer = 2.5f;
-    [SF] private Vector2Int _minMaxFollowerLoss = Vector2Int.one;
+    [SF] private int _followerLoss = 2;
+    [Space]
+    [SF] private bool _canBeRespawned = true;
+    [SF] private float _respawnTimer = 10f;
     [Space]
     [SF] private LayerMask _playerLayer = 1 << 0;
-    [SF] private PlayerStatContainer _stats = null;
-    [SF] private UpdateManager _update = null;
-    
-    private float _timer = 0f;
+    [SF] private PlayerStatContainer _playerStats = null;
+    [SF] private ParticleSystem _crashFX = null;
+    [SF] private AudioSource _crashSound = null;
+
+    private BoostController _boost = null;
+    private MovementController _movement = null;
+    private PlayerFollowers _followers = null;
     private readonly float _speedPercent = 0.5f;
 
 // COLLISION HANDLING
@@ -24,47 +28,63 @@ public class Hazard : MonoBehaviour
     private void OnCollisionEnter(Collision other){
         var layer = other.gameObject.layer;
         if (((1 << layer) & _playerLayer) == 0) return;
-        
-        // Timer between collisions
-        if (_timer > 0) return;
 
         var player = other.gameObject;
         var movement = GetMovement(player);
 
-        // Min speed to loose followers
-        var minSpeed = _stats.MaxMoveSpeed * _speedPercent;
+        var minSpeed = _playerStats.MaxMoveSpeed * _speedPercent;
         if (movement.Speed < minSpeed) return;
 
-        //  Always break hazard when boosting
-        var boost = GetBoost(player);
-        var canBreakHazard = boost.IsBoosting;
-        
-        if (_canBeDestroyed && canBreakHazard){
-            this.gameObject.SetActive(false);
-            return;
-        }
+        if (_crashFX != null) 
+            _crashFX.Play();
 
-        RemoveFollower(player, movement.Speed);
-        RunTimer(true);
+        if (_crashSound != null) 
+            _crashSound.Play();
+
+        var boost = GetBoost(player);
+        if (!boost.IsBoosting) 
+            RemoveFollower(player);
+
+        if (!_canBeDestroyed) return;
+        ToggleHazard(false);
+
+        if (!_canBeRespawned) return;
+        Invoke("Respawn", _respawnTimer);
     }
+        
+    /// <summary>
+    /// Changes the hazard's enabled state
+    /// </summary>
+    private void ToggleHazard(bool enabled){
+        var renderers = GetComponentsInChildren<MeshRenderer>();
+        
+        for (int i = 0; i < renderers.Length; i++)
+            renderers[i].enabled = enabled;
+
+        var collider = GetComponent<Collider>();
+        collider.enabled = enabled;
+    }
+
+    /// <summary>
+    /// Respawns the hazard, on invoke
+    /// </summary>
+    private void Respawn() => ToggleHazard(true);
+
+// PLAYER HANDLING
 
     /// <summary>
     /// Remove follower(s) from player
     /// </summary>
-    private void RemoveFollower(GameObject player, float speed){
-        var followers = player.GetComponent<PlayerFollowers>();
-
-        var minSpeed = (_stats.MaxMoveSpeed * _speedPercent);
-        var percent = Mathf.InverseLerp(minSpeed, _stats.MaxMoveSpeed, speed);
-        var count = (int)Mathf.Lerp(_minMaxFollowerLoss.x, _minMaxFollowerLoss.y, percent);
-
-        followers.Remove(count);
+    private void RemoveFollower(GameObject player){
+        _followers ??= player.GetComponent<PlayerFollowers>();
+        _followers.Remove(_followerLoss);
     }
 
     /// <summary>
     /// Returns the player movement controller
     /// </summary>
     private MovementController GetMovement(GameObject player){
+        if (_movement != null) return _movement;
         var controller = player.GetComponent<Player>().ControllerManager;
         return controller.GetController<MovementController>();
     }
@@ -73,34 +93,8 @@ public class Hazard : MonoBehaviour
     /// Returns the player boost controller
     /// </summary>
     private BoostController GetBoost(GameObject player){
+        if (_boost != null) return _boost;
         var controller = player.GetComponent<Player>().ControllerManager;
         return controller.GetController<BoostController>();
-    }
-
-// TIMER HANDLING
-
-    /// <summary>
-    /// Decrement collision timer
-    /// </summary>
-    /// <param name="deltaTime"></param>
-    private void OnUpdate(float deltaTime){
-        _timer -= deltaTime;
-
-        if (_timer > 0) return;
-        RunTimer(false);
-    }
-
-    /// <summary>
-    /// Toggles update loop
-    /// </summary>
-    private void RunTimer(bool run){
-        if (run){
-            _timer = _collisionTimer;
-            _update.Subscribe(OnUpdate, UpdateType.Update);
-        
-        } else {
-            _timer = 0;
-            _update.Unsubscribe(OnUpdate, UpdateType.Update);
-        }
     }
 }
